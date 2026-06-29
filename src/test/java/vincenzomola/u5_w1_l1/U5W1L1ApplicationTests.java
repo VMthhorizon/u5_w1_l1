665b72a7de09199f6f3be4b412b4b4731c4814cf
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-06-29 14:57:58 +0200

    Initial commit

---
+++ src/test/java/vincenzomola/u5_w1_l1/U5W1L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w1_l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
